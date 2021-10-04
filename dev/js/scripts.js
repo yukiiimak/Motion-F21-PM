d5beff0c1027080561892a92c91d752d37a3174a
yukiiimak <yukimak821@gmail.com>
2021-10-04 03:37:15 -0400

    js for hero

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,4 +2,6 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#hero article",{duration:2,alpha:0, scale:2});
+mainTL.from("#hero article", {duration:2, alpha:0, scale:2})
+    .from("h1", {duration:2, x:"+=500", alpha:0}, "-=50%")
+    .from("h2", {duration:2, x:"-=500", alpha:0}, "-=50%");
