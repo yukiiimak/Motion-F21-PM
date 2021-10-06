aebc61db5ada2eed0160fa0ded64b6d82952bc6f
yukiiimak <yukimak821@gmail.com>
2021-10-05 21:06:51 -0400

    animated stamp

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,3 +5,4 @@ const mainTL = gsap.timeline();
 mainTL.from("#hero article", {duration:2, alpha:0, scale:2})
     .from("h1", {duration:2, x:"+=500", alpha:0}, "-=50%")
     .from("h2", {duration:2, x:"-=500", alpha:0}, "-=50%")
+    .from("article div", {duration:1, y:"-=200", alpha:0, rotate:-90})
