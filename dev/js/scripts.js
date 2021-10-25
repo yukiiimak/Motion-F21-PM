3ca84ab36f31cca3b5ce72f4f603dcfa519d465c
yukiiimak <yukimak821@gmail.com>
2021-10-24 22:09:07 -0400

    changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,7 +2,12 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#hero article", {duration:2, alpha:0, scale:2})
-    .from("h1", {duration:2, x:"+=500", alpha:0}, "-=50%")
-    .from("h2", {duration:2, x:"-=500", alpha:0}, "-=50%")
-    .from("article div", {duration:1, y:"-=200", alpha:0, rotate:-90})
+mainTL.to("Cloud 1", {duration: 1, scale:3, y:"-=600"}); 
+
+
+
+
+// mainTL.from("#hero article", {duration:2, alpha:0, scale:2})
+//     .from("h1", {duration:2, x:"+=500", alpha:0}, "-=50%")
+//     .from("h2", {duration:2, x:"-=500", alpha:0}, "-=50%")
+//     .from("article div", {duration:1, y:"-=200", alpha:0, rotate:-90})
