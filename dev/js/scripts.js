5ec5b311665bd8166c61394e5026ce13e5563d4c
yukiiimak <yukimak821@gmail.com>
2021-11-01 13:30:13 -0400

    added motionpathplugin

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,8 @@
 import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
-gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(GSDevTools, MotionPathPlugin);
 
 const mainTL = gsap.timeline();
 
