4827cb68f40d8cf2d55fdd5c940bfef832a04dba
yukiiimak <yukimak821@gmail.com>
2021-11-10 12:54:35 -0500

    changed duration of functions

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,8 +4,8 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(GSDevTools, MotionPathPlugin);
 gsap.set("#Circle",{y:"+=15", transformOrigin:"center"});
-gsap.set("#Louvre", {y:"-30", transformOrigin:"bottom"}); 
-gsap.set("#Sydney-Opera-House", {y:"-16", transformOrigin:"bottom"}); 
+gsap.set("#Louvre", {y:"-=30", transformOrigin:"bottom"}); 
+gsap.set("#Sydney-Opera-House", {y:"-=16", transformOrigin:"bottom"}); 
 gsap.set("#Great-Wall", {y:"-=20", x:"-=3", transformOrigin:"bottom"}); 
 
 gsap.set("#preloader",{transformOrigin:"center bottom"});
@@ -14,48 +14,48 @@ const mainTL = gsap.timeline();
 
 function TL(){
     const tl =gsap.timeline(); 
-    tl.to("#Airplane", {duration:2, x:"+=50", y:"-=10", ease:"none"}, "same"); 
-    tl.to("#Airplane", {duration:2, x:"+=50", y:"+=30", ease:"none"});
-    tl.to("#Airplane", {duration:2, x:"+=50", y:"-=20", ease:"none"});
-    tl.to("#Airplane", {duration:2, x:"+=50", y:"+=20", ease:"none"});
-    tl.to("#Airplane", {duration:2, x:"+=50", ease:"none"});
-    tl.to("#Airplane", {duration:2, x:"+=83", y:"-=35", ease:"none"});
+    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"-=10", ease:"none"}, "same"); 
+    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"+=30", ease:"none"});
+    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"-=20", ease:"none"});
+    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"+=20", ease:"none"});
+    tl.to("#Airplane", {duration:2.5, x:"+=50", ease:"none"});
+    tl.to("#Airplane", {duration:2.5, x:"+=83", y:"-=35", ease:"none"});
     return tl; 
 }   
 
 function clouds(){
     const tl =gsap.timeline({repeat:-1}); 
-    tl.to("#Cloud-1", {duration:8, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-1-2", {duration:8, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-2", {duration:8, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-2-2", {duration:8, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-3", {duration:8, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-3-2", {duration:8, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-4", {duration:8, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-4-2", {duration:8, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-5", {duration:8, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-5-2", {duration:8, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-6", {duration:8, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-6-2", {duration:8, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-1", {duration:10, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-1-2", {duration:10, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-2", {duration:10, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-2-2", {duration:10, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-3", {duration:10, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-3-2", {duration:10, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-4", {duration:10, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-4-2", {duration:10, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-5", {duration:10, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-5-2", {duration:10, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-6", {duration:10, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-6-2", {duration:10, x:"+=585", ease:"none"}, "same");
 }
 
 function landmarks(){
     const tl =gsap.timeline(); 
-    tl.to ("#Louvre", {duration:4, motionPath:{path:"#Circle", align:"#Circle", alignOrigin:[0.3,0.8], autoRotate:true}}, "same")
-    tl.to ("#Great-Wall", {duration:4, motionPath:{path:"#Circle", align:"#Circle", alignOrigin:[0.4,0.7], autoRotate:true}})
-    tl.to ("#Sydney-Opera-House", {duration:4, motionPath:{path:"#Circle", align:"#Circle", alignOrigin:[0.3,0.7], autoRotate:true}}); 
+    tl.to ("#Louvre", {duration:5, motionPath:{path:"#Circle", align:"#Circle", alignOrigin:[0.3,0.8], autoRotate:true}, rotate:180})
+    tl.to ("#Great-Wall", {alphaAuto:1, duration:5, motionPath:{path:"#Circle", align:"#Circle", alignOrigin:[0.4,0.7], autoRotate:true}, rotate:180})
+    tl.to ("#Sydney-Opera-House", {alphaAuto:1, duration:5, motionPath:{path:"#Circle", align:"#Circle", alignOrigin:[0.3,0.7], autoRotate:true}, rotate:180}); 
 }
 
-function heroAnimation(){
-    const tl =gsap.timeline();
-    const heroHeight = document.querySelector("#hero"); 
-    tl.to ("#hero", {duration:5, alpha:0, y:heroHeight.clientHeight}); 
-}
+// function heroAnimation(){
+//     const tl =gsap.timeline();
+//     const heroHeight = document.querySelector("#hero"); 
+//     tl.to ("#hero", {duration:5, alpha:0, y:heroHeight.clientHeight}); 
+// }
 
 
 landmarks();
 clouds();
-heroAnimation();
+// heroAnimation();
 mainTL.add(TL()); 
 
 GSDevTools.create();
