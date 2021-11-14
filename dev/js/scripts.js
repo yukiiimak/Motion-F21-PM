6b740c48f69ec4b2de91d632b2dff0445fdde882
yukiiimak <yukimak821@gmail.com>
2021-11-13 19:30:53 -0500

    made preloader fade into hero with hero animations

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -25,18 +25,18 @@ function TL(){
 
 function clouds(){
     const tl =gsap.timeline({repeat:-1}); 
-    tl.to("#Cloud-1", {duration:14, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-1-2", {duration:14, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-2", {duration:14, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-2-2", {duration:14, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-3", {duration:14, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-3-2", {duration:14, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-4", {duration:14, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-4-2", {duration:14, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-5", {duration:14, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-5-2", {duration:14, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-6", {duration:14, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-6-2", {duration:14, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-1", {duration:15, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-1-2", {duration:15, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-2", {duration:15, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-2-2", {duration:15, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-3", {duration:15, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-3-2", {duration:15, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-4", {duration:15, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-4-2", {duration:15, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-5", {duration:15, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-5-2", {duration:15, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-6", {duration:15, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-6-2", {duration:15, x:"+=585", ease:"none"}, "same");
 }
 
 function landmarks(){
@@ -59,27 +59,28 @@ function landmarks(){
     tl.from ("#Shell-4", {duration:0.1, autoAlpha:0, delay:1.4}, "turn-3")
     tl.from ("#Shell-5", {duration:0.1, autoAlpha:0, delay:1.6}, "turn-3")
     tl.from ("#Shell-6", {duration:0.1, autoAlpha:0, delay:1.8}, "turn-3");
-    
-    // tl.from ("#Sydney-Opera-House", {autoAlpha:0, duration:5, motionPath:{path:"#Circle", align:"#Circle", alignOrigin:[0.3,0.7]}}); 
+    tl.to ("#preloader", {duration:2, alpha:0, onComplete:backToTop}); 
 }
 
-// function heroAnimation(){
-//     const tl =gsap.timeline();
-//     const heroHeight = document.querySelector("#hero"); 
-//     tl.to ("#hero", {duration:5, alpha:0, y:heroHeight.clientHeight}); 
-// }
+function backToTop(){
+    window.scrollTo(0,0)
+    gsap.set("#preloader", {display:"none"})
+}
+
+function heroAnimation(){
+    const tl = gsap.timeline();
+    tl.from("#hero article", {duration:2, alpha:0, scale:2})
+    .from("h1", {duration:2, x:"+=500", alpha:0}, "-=50%")
+    .from("h2", {duration:2, x:"-=500", alpha:0}, "-=50%")
+    .from("article div", {duration:1, y:"-=200", alpha:0, rotate:-90})
+    return tl; 
+}
 
 
-mainTL.add(TL()); 
-clouds();
-landmarks();
-// heroAnimation();
+mainTL.add(TL())
+.add(clouds())
+.add(landmarks())
+.add(heroAnimation());
 
 GSDevTools.create();
 MotionPathPlugin.create();
-
-
-// mainTL.from("#hero article", {duration:2, alpha:0, scale:2})
-//     .from("h1", {duration:2, x:"+=500", alpha:0}, "-=50%")
-//     .from("h2", {duration:2, x:"-=500", alpha:0}, "-=50%")
-//     .from("article div", {duration:1, y:"-=200", alpha:0, rotate:-90})
