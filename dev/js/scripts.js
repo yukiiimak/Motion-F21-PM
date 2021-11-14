b6d33c4e34f6a1c48b873d89fdfc612831f03c8a
yukiiimak <yukimak821@gmail.com>
2021-11-13 19:03:13 -0500

    increased duration of airplane and clouds

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,29 +14,29 @@ const mainTL = gsap.timeline();
 
 function TL(){
     const tl =gsap.timeline(); 
-    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"-=10", ease:"none"}, "same"); 
-    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"+=30", ease:"none"});
-    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"-=20", ease:"none"});
-    tl.to("#Airplane", {duration:2.5, x:"+=50", y:"+=20", ease:"none"});
-    tl.to("#Airplane", {duration:2.5, x:"+=50", ease:"none"});
-    tl.to("#Airplane", {duration:2.5, x:"+=83", y:"-=35", ease:"none"});
+    tl.to("#Airplane", {duration:3.2, x:"+=50", y:"-=10", ease:"none"}, "same"); 
+    tl.to("#Airplane", {duration:3.2, x:"+=50", y:"+=30", ease:"none"});
+    tl.to("#Airplane", {duration:3.2, x:"+=50", y:"-=20", ease:"none"});
+    tl.to("#Airplane", {duration:3.2, x:"+=50", y:"+=20", ease:"none"});
+    tl.to("#Airplane", {duration:3.2, x:"+=50", ease:"none"});
+    tl.to("#Airplane", {duration:3.2, x:"+=83", y:"-=35", ease:"none"});
     return tl; 
 }   
 
 function clouds(){
     const tl =gsap.timeline({repeat:-1}); 
-    tl.to("#Cloud-1", {duration:10, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-1-2", {duration:10, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-2", {duration:10, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-2-2", {duration:10, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-3", {duration:10, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-3-2", {duration:10, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-4", {duration:10, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-4-2", {duration:10, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-5", {duration:10, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-5-2", {duration:10, x:"+=585", ease:"none"}, "same");
-    tl.to("#Cloud-6", {duration:10, x:"-=585", ease:"none"}, "same")
-    .from("#Cloud-6-2", {duration:10, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-1", {duration:14, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-1-2", {duration:14, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-2", {duration:14, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-2-2", {duration:14, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-3", {duration:14, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-3-2", {duration:14, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-4", {duration:14, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-4-2", {duration:14, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-5", {duration:14, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-5-2", {duration:14, x:"+=585", ease:"none"}, "same");
+    tl.to("#Cloud-6", {duration:14, x:"-=585", ease:"none"}, "same")
+    .from("#Cloud-6-2", {duration:14, x:"+=585", ease:"none"}, "same");
 }
 
 function landmarks(){
